1bd152a61437dceb3dbb5900c03af87f0c552c95
Captain Fantasy <captain@fantasyui.com>
2020-04-20 18:55:08 -0400

    Code Cleanup

--- update-module.mjs
+++ update-module.mjs
@@ -1,48 +1,38 @@
 #!/usr/bin/env -S node --experimental-modules
-
-import fs from 'fs';
+import fs from 'fs-extra';
+import path from 'path';
 import matter from 'gray-matter';
-//import byKey from 'natural-sort-by-key';
 import byKey from '../natural-sort-by-key/index.js';
 import marked from 'marked';
 import markedHelpers from 'marked/src/helpers.js';
-
 const { cleanUrl, escape } = markedHelpers;
-
 // Get reference
 const renderer = new marked.Renderer();
-
 // Override function
 renderer.image = function(href, title, text) {
     href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
     if (href === null) {
       return text;
     }
-
     let out = '<img class="img-fluid rounded shadow mt-5 mb-2" src="' + href + '" alt="' + text + '"';
     if (title) {
       out += ' title="' + title + '"';
     }
     out += this.options.xhtml ? '/>' : '>';
     return out;
-  }
+};
 
+const files = fs.readdirSync('./news/', { withFileTypes: true }).filter(o=>o.isFile()).filter(o=>o.name.match(/^id[0-9]+\.md/)).map(o=>path.resolve('./news',o.name));
+const dirs = fs.readdirSync('./news/', { withFileTypes: true }).filter(o=>o.isDirectory()).filter(o=>o.name.match(/^[^._]/)).map(o=>path.resolve('./news',o.name));
 
-//  const order = JSON.parse(fs.readFileSync(`db/data/order.json`).toString()); // .map({}) // .sort( byKey('date') )
-const order = fs.readdirSync('db/content/posts/');
-
-//console.log(order);
+console.log(dirs);
 
 let list = [];
-
-for( let id of order){
-  const {content, data} = matter(fs.readFileSync(`db/content/posts/${id}`).toString());
+for( let id of files){
+  const {content, data} = matter(fs.readFileSync(id).toString());
   list.push(Object.assign({},data,{html:marked(content, { renderer })}));
 }
-
 list = list.sort(function(a,b){
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
   return new Date(b.date) - new Date(a.date);
 });
 let meta = `
@@ -52,5 +42,8 @@ export default function main(){
   return data;
 }
 `;
-
 fs.writeFileSync(`src/modules/db/index.js`,  meta);
+
+for( let id of dirs){
+  fs.copySync(id, './static/', {preserveTimestamps:true})
+}
---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 git@github.com:fantasyui-com/news.git
+
+./update-module.mjs
