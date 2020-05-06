81688a7a4a6fa44dab8179c3b9573060f2ea3957
Captain Fantasy <captain@fantasyui.com>
2020-05-06 12:03:10 -0400

    Clone correct repositories

--- update.sh
+++ update.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-if [ -d news ]; then
-  cd news
+# if [ -d news ]; then
+#   cd news
+#   git pull
+#   cd ..
+# else
+#   git clone --depth 1 git@github.com:fantasyui-com/news.git
+# fi
+
+
+
+if [ -d poetry ]; then
+  cd poetry
   git pull
   cd ..
 else
-  git clone --depth 1 git@github.com:fantasyui-com/news.git
+  git clone --depth 1 git@github.com:catpea/poetry.git
+fi
+if [ -d research ]; then
+  cd research
+  git pull
+  cd ..
+else
+  git clone --depth 1 git@github.com:catpea/research.git
 fi
 
-./update-module.mjs
+
+#./update-module.mjs
